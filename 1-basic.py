a3a226beffefff6b4358f0d17baab6acdb493ea4
giangdx <giangdx@meeyland.com>
2025-09-30 11:33:23 +0700

    first commit

---
+++ 1-basic.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+client = OpenAI(
+    api_key=api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+    )
+
+user_prompt = input("Hãy nhập câu hỏi: ")
+
+completion = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[
+        {"role":"system", "content":"You are a helpful assistant."},
+        {
+            "role":"user",
+            "content" : f"{user_prompt}"
+        },
+    ],
+)
+
+response = completion.choices[0].message.content
+
+print(response)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Building AI Agents in Pure Python - Beginner Course
+
+## NOTE: Cách tốt nhất để xây dựng AI Agent là làm việc trực tiếp với API (call API) của các model LLMs 
+
+ 
